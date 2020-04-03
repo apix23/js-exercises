d7de4916484069d7649deccb695cca66efe4b09e
Migracode <sanchezgalejandroj@gmail.com>
2020-04-03 21:36:23 +0200

    commit for L-functions-parameters exercises

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -1,5 +1,8 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var totalSum = numberOfStudents+numberOfMentors;
+var percentOfStudents = Math.round(numberOfStudents * 100 / totalSum);
+var percentOfMentors = Math.round(numberOfMentors * 100 / totalSum);
+
+console.log("Percentage students: "+ percentOfStudents+"%");
+console.log("percentage mentors: " + percentOfMentors+"%");
---
+++ week-1/L-functions-nested/exercise2.js
@@ -0,0 +1,19 @@
+var mentor1 = "Daniel";
+var mentor2 = "Irina";
+var mentor3 = "Mimi";
+var mentor4 = "Rob";
+var mentor5 = "Yohannes";
+
+function upperCaseName(name) {
+    return name.toUpperCase();
+}
+
+function shoutyGreeting(name) {
+    return "HELLO "+ upperCaseName(name);
+}
+
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
