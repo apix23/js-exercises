bedff54438780e24cb1535edbdb1b427298fd788
Migracode <sanchezgalejandroj@gmail.com>
2020-04-03 20:06:23 +0200

    exercise from I-floats complete

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var totalSum = numberOfStudents+numberOfMentors;
+var percentOfStudents = Math.round(numberOfStudents * 100 / totalSum);
+var percentOfMentors = Math.round(numberOfMentors * 100 / totalSum);
+
+console.log("Percentage students: "+ percentOfStudents+"%");
+console.log("percentage mentors: " + percentOfMentors+"%");
+
