13513b4c167470a73a6b844b1aed9538ef8b67e6
Migracode <sanchezgalejandroj@gmail.com>
2020-04-03 19:49:13 +0200

    exercise 2 of G-string-method complete and commit

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,7 @@
 var name = " Daniel  ";
 
 console.log(message);
+var name = "Alejandro";
+var lengthName = name.length;
+var message = "My name is Alejandro and my name is "+lengthName+ " characters long"
+console.log(message.trim());
