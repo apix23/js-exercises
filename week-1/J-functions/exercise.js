a097b1806a46d9fb996d218963912b1fd39089cd
Migracode <sanchezgalejandroj@gmail.com>
2020-04-03 20:13:13 +0200

    exercise 1 and 2 from I-floats complete and commit

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -1,7 +1,16 @@
 function halve(number) {
   // complete the function here
+   return number/2;
 }
 
 var result = halve(12);
+
+console.log(result);
+
+result = halve(36);
+
+console.log(result);
+
+result = halve(19344057);
 
 console.log(result);
--- week-1/J-functions/exercise2.js
+++ week-1/J-functions/exercise2.js
@@ -1,7 +1,14 @@
 function triple(number) {
   // complete function here
+  return number * 3;
 }
 
 var result = triple(12);
 
 console.log(result);
+
+result = triple(49);
+
+console.log(result);
+
+console.log(triple(23));
