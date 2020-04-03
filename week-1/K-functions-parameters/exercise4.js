e4e0ce47a0d6b160ee37cb59c941a6908e9a8941
Migracode <sanchezgalejandroj@gmail.com>
2020-04-03 21:17:52 +0200

    all exercises from k-functions-parameters completes

--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -1,5 +1,7 @@
 // Declare your function first
-
+function sumOfTwoNumbers(a,b) {
+    return a+b;
+}
 // Call the function and assign to a variable `sum`
-
+var sum = sumOfTwoNumbers(13,124);
 console.log(sum);
